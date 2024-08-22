f5dc81874e84f426ebede09035f1e7dfc05652c0
Shahin Rahimi <sh.rahimi.dev@gmail.com>
2024-08-22 14:21:11 +0330

    update

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module gihub.com/shahinrahimi/teletradebot
 
 go 1.23.0
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/url"
 
-func main(){
-	fmt.Println("Hello")
+	"github.com/gorilla/websocket"
+)
+
+func main() {
+	// Define the WebSocket server URL
+	serverURL := "wss://ws.bitmex.com/realtimePlatform"
+
+	// Parse the URL
+	u, err := url.Parse(serverURL)
+	if err != nil {
+		log.Fatal("Error parsing URL:", err)
+	}
+
+	// Create a new WebSocket dialer
+	dialer := websocket.DefaultDialer
+
+	// Connect to the WebSocket server
+	conn, _, err := dialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("Error connecting to WebSocket server:", err)
+	}
+	defer conn.Close()
+
+	log.Printf("Connected to %s\n", serverURL)
 }
