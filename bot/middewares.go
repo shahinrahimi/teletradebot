43167850f09b9c355f025154be12a1ba12e13089
Shahin Rahimi <sh.rahimi.dev@gmail.com>
2024-08-25 22:24:43 +0330

    implement shortcut

--- bot/middewares.go
+++ bot/middewares.go
@@ -3,6 +3,7 @@ package bot
 import (
 	"context"
 	"fmt"
+	"net/http"
 	"strconv"
 	"strings"
 
@@ -23,16 +24,29 @@ func (b *Bot) Logger(next Handler) Handler {
 
 func (b *Bot) ProvideAddOrder(next Handler) Handler {
 	return func(u *tgbotapi.Update, ctx context.Context) {
+		var tradeArgs []string
 		args := strings.Split(u.Message.CommandArguments(), " ")
-		o, err := ParseOrder(args)
+		// check shortcuts
+		if len(args) == 1 {
+			value, exist := Shortcuts[args[0]]
+			if !exist {
+				b.SendMessage(u.Message.From.ID, fmt.Sprintf("shortcut is not found: '%s'", args[0]))
+				return
+			}
+			tradeArgs = strings.Split(value, " ")
+		} else {
+			tradeArgs = args
+		}
+		o, err := ParseOrder(tradeArgs)
 		if err != nil {
 			b.SendMessage(u.Message.From.ID, err.Error())
 			return
 		}
-		// check pair for binance
+
 		var isAvailable bool = false
 		switch o.Account {
 		case models.ACCOUNT_B:
+			// check pair for binance
 			for _, s := range b.bc.Symbols {
 				if s.Symbol == strings.ToUpper(o.Pair) {
 					isAvailable = true
@@ -40,10 +54,16 @@ func (b *Bot) ProvideAddOrder(next Handler) Handler {
 				}
 			}
 		case models.ACCOUNT_M:
+			// check pair for bitmex
 			// TODO implement a method to check if pair is available in bitmex
-			isAvailable = true
+			c := http.Client{}
+			resp, err := c.Get(fmt.Sprintf("https://www.bitmex.com/api/udf/symbols?symbol=%s", o.Pair))
+			if resp.StatusCode == 200 || err != nil {
+				isAvailable = true
+			}
 		default:
 			// should never happen
+
 			b.l.Panicf("error checking pair availability pair: %s", o.Pair)
 
 		}
@@ -58,15 +78,15 @@ func (b *Bot) ProvideAddOrder(next Handler) Handler {
 	}
 }
 
-func ParseOrder(args []string) (*models.Order, error) {
+func ParseOrder(tradeArgs []string) (*models.Order, error) {
 	var o models.Order
-	if len(args) < 9 {
+	if len(tradeArgs) < 9 {
 		return nil, fmt.Errorf("the length of args is not sufficient for parsing")
 	}
 	// account it should be string
 	// m for bitmex
 	// b for binance
-	part1 := strings.TrimSpace(strings.ToLower(args[0]))
+	part1 := strings.TrimSpace(strings.ToLower(tradeArgs[0]))
 	if len(part1) > 1 || (part1 != "m" && part1 != "b") {
 		return nil, fmt.Errorf("the valid value for account should be 'm' => bitmex, 'b' => binance")
 	} else if part1 == "m" {
@@ -79,24 +99,24 @@ func ParseOrder(args []string) (*models.Order, error) {
 	}
 	// pair
 	// TODO maybe add check if pair exist on the tickers
-	part2 := strings.TrimSpace(strings.ToUpper(args[1]))
+	part2 := strings.TrimSpace(strings.ToUpper(tradeArgs[1]))
 	o.Pair = part2
 	// side
-	part3 := strings.TrimSpace(strings.ToUpper(args[2]))
+	part3 := strings.TrimSpace(strings.ToUpper(tradeArgs[2]))
 	if part3 != models.SIDE_L && part3 != models.SIDE_S {
 		return nil, fmt.Errorf("the valid value for side should be 'long' or 'short'")
 	} else {
 		o.Side = part3
 	}
 	// pair
-	part4 := strings.TrimSpace(strings.ToLower(args[3]))
+	part4 := strings.TrimSpace(strings.ToLower(tradeArgs[3]))
 	if part4 != models.CANDLE_15MIN && part4 != models.CANDLE_30MIN && part4 != models.CANDLE_1H && part4 != models.CANDLE_4H {
 		return nil, fmt.Errorf("the valid value for candle should be '15min', '30min', '1h' or '4h'")
 	} else {
 		o.Candle = part4
 	}
 	// offset
-	part5 := strings.TrimSpace(args[4])
+	part5 := strings.TrimSpace(tradeArgs[4])
 	offset, err := strconv.ParseFloat(part5, 64)
 	if err != nil {
 		return nil, fmt.Errorf("the valid value for offset_entry should be amount (float or integer)")
@@ -104,7 +124,7 @@ func ParseOrder(args []string) (*models.Order, error) {
 		o.Offset = float32(offset)
 	}
 	// size percent
-	part6 := strings.TrimSpace(args[5])
+	part6 := strings.TrimSpace(tradeArgs[5])
 	size_percent, err := strconv.Atoi(part6)
 	if err != nil {
 		return nil, fmt.Errorf("the valid value for size should be amount in percent (e.g 5)")
@@ -115,7 +135,7 @@ func ParseOrder(args []string) (*models.Order, error) {
 	}
 
 	// stop-loss percent
-	part7 := strings.TrimSpace(args[6])
+	part7 := strings.TrimSpace(tradeArgs[6])
 	stop_percent, err := strconv.Atoi(part7)
 	if err != nil {
 		return nil, fmt.Errorf("the valid value for stop-loss percent should be amount in percent (e.g 105)")
@@ -126,7 +146,7 @@ func ParseOrder(args []string) (*models.Order, error) {
 	}
 
 	// target-point percent
-	part8 := strings.TrimSpace(args[7])
+	part8 := strings.TrimSpace(tradeArgs[7])
 	target_percent, err := strconv.Atoi(part8)
 	if err != nil {
 		return nil, fmt.Errorf("the valid value for target-point percent should be amount in percent (e.g 105)")
@@ -137,7 +157,7 @@ func ParseOrder(args []string) (*models.Order, error) {
 	}
 
 	// reverse-multiplier
-	part9 := strings.TrimSpace(args[8])
+	part9 := strings.TrimSpace(tradeArgs[8])
 	reverse_multiplier, err := strconv.Atoi(part9)
 	if err != nil {
 		return nil, fmt.Errorf("the valid value for reverse_multiplier should be number (1 or 2)")
