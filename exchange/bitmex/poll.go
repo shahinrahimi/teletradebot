41ed4f251252fe41109402a165d12230b2e885c1
Shahin Rahimi <sh.rahimi.dev@gmail.com>
2024-09-24 14:06:00 +0330

    add rate limiter for bitmex

--- exchange/bitmex/poll.go
+++ exchange/bitmex/poll.go
@@ -3,6 +3,14 @@ package bitmex
 import (
 	"context"
 	"time"
+
+	"github.com/shahinrahimi/teletradebot/swagger"
+)
+
+var (
+	initialBackoff = 5 * time.Second
+	maxBackOff     = 5 * time.Minute
+	backoff        = initialBackoff
 )
 
 func (mc *BitmexClient) StartPolling(ctx context.Context) {
@@ -17,7 +25,20 @@ func (mc *BitmexClient) pollActiveInstruments(ctx context.Context, interval time
 		instruments, _, err := mc.client.InstrumentApi.InstrumentGetActive(ctx)
 		if err != nil {
 			mc.l.Printf("Error fetching exchange info: %v", err)
-			continue
+			if apiErr, ok := err.(swagger.GenericSwaggerError); ok {
+				if apiErr.Error() == "429 Too Many Requests" {
+					mc.l.Printf("hit bitmex rate limit: %+v", apiErr.Body())
+					time.Sleep(backoff)
+					if backoff > maxBackOff {
+						backoff = maxBackOff
+					} else {
+						backoff *= 2
+					}
+					continue
+				}
+			} else {
+				mc.l.Panicf("error fetching exchange info: %v", err)
+			}
 		}
 		mc.activeInstruments = instruments
 		time.Sleep(interval)
